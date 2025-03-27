6fd544459f07b5a24cf05200d508a23a9d33057f
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-27 15:36:14 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+/*
+Script für die neuseelandreise
+*/
+//Karte initialisieren
 var map = L.map('map').setView([-38.690, 176.080], 11);
+//Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+    //MArker zeichnen
         }).addTo(map);
         var marker = L.marker([-38.690, 176.080]).addTo(map);
+
+        //Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
