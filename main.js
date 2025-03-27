ba3823e0ed0f893455a477b4d11077b1d9d4228c
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-27 16:24:04 +0100

    let Stop definiert

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ let lat = -38.690;
 let lng = 176.080;
 let zoom = 11;
 
+let stop = {
+    nr.: 17,
+    title: Taupo,
+    user: johannauniibk,
+    lat: -38.690,
+    lng: 176.080,
+    zoom: 11,
+}
+
 //Karte initialisieren
 let map = L.map('map').setView([lat, lng], zoom);
 
@@ -19,7 +28,21 @@ let marker = L.marker([-38.690, 176.080]).addTo(map);
 
 //Popup definieren und öffnen
 marker.bindPopup(`
-            <b>Hello world!</b>
-            <br>
-            I am a popup at ${lat.toFixed(5)} / ${lng.toFixed(5)}.
+            <h2>Taupo</h2>
+            <ul>
+                <li>Geogr. Breite: ${lat.toFixed(5)}° </li>
+                <li>Geogr. Länge: ${lng.toFixed(5)}°</li>
+            </ul>
+          
         `).openPopup();
+
+        let coarse ={
+                title:"Taupo,",
+                semester: "25S",
+                Stunden: 3,
+                typ: "VU",
+        };
+        console.log("title", course.title);
+        console.log("semester", course.semester)
+        console.log("Stunden", course.Stunden)
+        console.log
