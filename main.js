f6071e6c3b2ea74c81c51257c6509ee5d4f4ca20
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-03-27 18:31:20 +0100

    Eigene Etappe als erstes im Pulldown

--- main.js
+++ main.js
@@ -233,6 +233,16 @@ if(STOPS[i].user=="johannauniibk"){
     map.setView([STOPS[i].lat, STOPS[i].lng], STOPS[i].zoom);
     marker.openPopup();
 }
+//Pulldown Menü befüllen
+let option = document.createElement("option");
+option.value = STOPS[i].user;
+option.text = STOPS[i].title;
+
+if (STOPS[i].user == "johannauniibk"){
+    option.selected = true;
+}
+document.querySelector("#pulldown select").appendChild(option);
+
 }
 
 
